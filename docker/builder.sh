f10e7216da0ccc7cb18626a4cd2a8a269c0d1f95
Chris.Zhang <wfish34@gmail.com>
2026-04-23 22:52:47 +0800

    适配K8S

--- docker/builder.sh
+++ docker/builder.sh
@@ -1,8 +1,10 @@
 docker build -f docker/Dockerfile.user-service -t harbor-srv01.mylab021.com/ik8s-go-ms-proxy/user-service:latest .
 docker build -f docker/Dockerfile.order-service -t harbor-srv01.mylab021.com/ik8s-go-ms-proxy/order-service:latest .
+docker build -f docker/Dockerfile.product-service -t harbor-srv01.mylab021.com/ik8s-go-ms-proxy/product-service:latest .
 docker build -f docker/Dockerfile.gateway -t harbor-srv01.mylab021.com/ik8s-go-ms-proxy/gateway:latest .
 
 
 docker push harbor-srv01.mylab021.com/ik8s-go-ms-proxy/user-service:latest
 docker push harbor-srv01.mylab021.com/ik8s-go-ms-proxy/order-service:latest
+docker push harbor-srv01.mylab021.com/ik8s-go-ms-proxy/product-service:latest
 docker push harbor-srv01.mylab021.com/ik8s-go-ms-proxy/gateway:latest
